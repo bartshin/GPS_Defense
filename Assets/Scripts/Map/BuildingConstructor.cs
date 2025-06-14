6d9c81f80264f72b15ce531c5ae219f57936cde6
bart <bartshin@icloud.com>
2025-06-14 14:15:32 +0900

    [Feat] Add building uv

--- Assets/Scripts/Map/BuildingConstructor.cs
+++ Assets/Scripts/Map/BuildingConstructor.cs
@@ -7,13 +7,28 @@ using OSM;
 
 public class BuildingConstructor : _MonoBehaviour
 {
+  struct MeshData
+  {
+    public List<Vector3> Vertices;
+    public List<int> Triangles;
+    public List<Vector3> Normals;
+    public List<Vector2> Uvs;
+      
+    public MeshData(int boundaryCount) 
+    {
+      this.Vertices = new List<Vector3>((boundaryCount - 1) * 4 + 1);
+      this.Triangles = new List<int>((boundaryCount - 1) * (12 + 3));
+      this.Normals = new List<Vector3>((boundaryCount - 1) * 4);
+      this.Uvs = new List<Vector2>();
+    }
+  }
   [SerializeField]
   Material buildingMaterial;
   [SerializeField] [Range(1f, 10f)]
   float minHeight = 2f;
   Vector3[] tempVertices = new Vector3[4];
-  //int[] tempBuildingTriangles = new int[12];
-  int[] tempBuildingTriangles = new int[6];
+  int[] tempTriangles = new int[6];
+  Vector2[] tempUvs = new Vector2[4];
 
   public GameObject Construct(OsmWay way, Vector3 mapCenter)
   {
@@ -40,30 +55,33 @@ public class BuildingConstructor : _MonoBehaviour
   void FillBuildingMeshData(Mesh mesh, OsmWay way, Vector2 buildingCenter)
   {
     var height = Math.Max(way.Height, this.minHeight);
-    var (vertices, triangles, normals) = this.CollectGeometries(
+    var meshData = new MeshData(way.Nodes.Count);
+    this.CollectGeometries(
+      data: meshData,
       way: way,
       buildingCenter: buildingCenter,
-      height: height);
+      height: height
+      );
     this.AddTopGeomeries(
-      vertices: vertices,
-      triangles: triangles,
-      normals: normals,
+      data: meshData,
       buildingCenter: buildingCenter,
+      buildingBounds: way.Bounds,
       height: height,
       wayCount: way.Nodes.Count);
-    mesh.vertices = vertices.ToArray();
-    mesh.triangles = triangles.ToArray();
-    mesh.normals = normals.ToArray();
+    mesh.vertices = meshData.Vertices.ToArray();
+    mesh.triangles = meshData.Triangles.ToArray();
+    mesh.normals = meshData.Normals.ToArray();
+    mesh.uv = meshData.Uvs.ToArray();
   }
 
-  (List<Vector3> vertices, List<int> triangles, List<Vector3> normals)
-    CollectGeometries(OsmWay way, Vector2 buildingCenter, float height)
+  void CollectGeometries(MeshData data, OsmWay way, Vector2 buildingCenter, float height)
   {
+    this.tempUvs[0] = new Vector2(0, 0);
+    this.tempUvs[1] = new Vector2(1, 0);
+    this.tempUvs[2] = new Vector2(0, 1);
+    this.tempUvs[3] = new Vector2(1, 1);
     var heightVector = new Vector3(0, height, 0);
     int triangleOffset = 0;
-    var vertices = new List<Vector3>((way.Nodes.Count - 1) * 4 + 1);
-    var triangles = new List<int>((way.Nodes.Count - 1) * (12 + 3));
-    var normals = new List<Vector3>((way.Nodes.Count - 1) * 4);
     var count = way.Nodes.Count;
     for (int i = 1; i < count; ++i) {
       this.tempVertices[0] = new Vector3(
@@ -79,50 +97,65 @@ public class BuildingConstructor : _MonoBehaviour
       this.tempVertices[2] = this.tempVertices[0] + heightVector;
       this.tempVertices[3] = this.tempVertices[1] + heightVector; 
 
-      normals.Add(-Vector3.forward);
-      normals.Add(-Vector3.forward);
-      normals.Add(-Vector3.forward);
-      normals.Add(-Vector3.forward);
+      data.Normals.Add(-Vector3.forward);
+      data.Normals.Add(-Vector3.forward);
+      data.Normals.Add(-Vector3.forward);
+      data.Normals.Add(-Vector3.forward);
 
       triangleOffset = 4 * (i - 1);
 
-      this.tempBuildingTriangles[0] = triangleOffset + 0;
-      this.tempBuildingTriangles[1] = triangleOffset + 2;
-      this.tempBuildingTriangles[2] = triangleOffset + 1;
+      this.tempTriangles[0] = triangleOffset + 0;
+      this.tempTriangles[1] = triangleOffset + 2;
+      this.tempTriangles[2] = triangleOffset + 1;
 
-      this.tempBuildingTriangles[3] = triangleOffset + 2;
-      this.tempBuildingTriangles[4] = triangleOffset + 3;
-      this.tempBuildingTriangles[5] = triangleOffset + 1;
+      this.tempTriangles[3] = triangleOffset + 2;
+      this.tempTriangles[4] = triangleOffset + 3;
+      this.tempTriangles[5] = triangleOffset + 1;
 
-      vertices.AddRange(this.tempVertices);
-      triangles.AddRange(this.tempBuildingTriangles);
+      data.Vertices.AddRange(this.tempVertices);
+      data.Triangles.AddRange(this.tempTriangles);
+      data.Uvs.AddRange(this.tempUvs);
     }
-    return (vertices, triangles, normals);
   }
 
   void AddTopGeomeries(
-    List<Vector3> vertices, 
-    List<int> triangles,
-    List<Vector3> normals,
+    MeshData data,
     Vector2 buildingCenter,
+    OsmBounds buildingBounds,
     float height,
     int wayCount)
   {
     Vector3 topCenter = new Vector3(0, height, 0);
-    Vector3 v1 = Vector3.zero;
-    Vector3 v2 = Vector3.zero;
+    Vector2 range = buildingBounds.CalcRange();
+    Vector2 centerUv = new Vector2(0.5f, 0.5f);
     for (int i = 1; i < wayCount; i++) {
       int baseIndex = 4 * (i - 1);
+      this.tempVertices[0] = data.Vertices[baseIndex + 3];
+      this.tempVertices[1] = data.Vertices[baseIndex + 2];
 
-      vertices.Add(vertices[baseIndex + 3]); 
-      vertices.Add(vertices[baseIndex + 2]); 
-      vertices.Add(topCenter);
-      triangles.Add(vertices.Count - 3); 
-      triangles.Add(vertices.Count - 2); 
-      triangles.Add(vertices.Count - 1); 
-      normals.Add(Vector3.up);
-      normals.Add(Vector3.up);
-      normals.Add(Vector3.up);
+      data.Vertices.Add(this.tempVertices[0]); 
+      data.Vertices.Add(this.tempVertices[1]); 
+      data.Vertices.Add(topCenter);
+      data.Triangles.Add(data.Vertices.Count - 3); 
+      data.Triangles.Add(data.Vertices.Count - 2); 
+      data.Triangles.Add(data.Vertices.Count - 1); 
+      data.Normals.Add(Vector3.up);
+      data.Normals.Add(Vector3.up);
+      data.Normals.Add(Vector3.up);
+      
+      data.Uvs.Add(
+        new Vector2(
+          this.tempVertices[0].x / range.x + 0.5f,
+          this.tempVertices[0].z / range.y + 0.5f
+          )
+        );
+      data.Uvs.Add(
+        new Vector2(
+          this.tempVertices[1].x / range.x + 0.5f,
+          this.tempVertices[1].z / range.y + 0.5f
+          )
+        );
+      data.Uvs.Add(centerUv);
     }
   }
 
--- Assets/Scripts/OSM/OsmBounds.cs
+++ Assets/Scripts/OSM/OsmBounds.cs
@@ -17,6 +17,22 @@ namespace OSM
     public float MaxLongitude { get; private set; }
     [SerializeField]
     public Vector2 Center { get; private set; }
+    
+    public Vector2 CalcRange() 
+    {
+      Vector2 xRange = new Vector2(
+        (float)(MercatorProjection.lonToX(this.MinLongitude)),
+        (float)(MercatorProjection.lonToX(this.MaxLongitude))
+        );
+      Vector2 yRange = new Vector2(
+        (float)(MercatorProjection.latToY(this.MinLatitude)),
+        (float)(MercatorProjection.latToY(this.MaxLatitude))
+        );
+      return (new Vector2(
+          xRange.y - xRange.x,
+          yRange.y - yRange.x
+          ));
+    }
 
     public OsmBounds(
       float minLatitude,
